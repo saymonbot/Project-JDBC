625f719356fb169ebe7ace29a403d25aa3331575
Saymon Botelho <saymon_botelho123@outlook.com>
2024-01-30 02:48:49 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -13,17 +14,21 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		Seller seller = sellerDao.findById(3);
-		System.out.println("TESTE 01 " + seller);
+		System.out.println("TESTE 01 - " + seller);
 		
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		for(Seller obj : list) {
-			System.out.println("TESTE 02 " + obj);
+			System.out.println("TESTE 02 - " + obj);
 		}
 		
 		list = sellerDao.findAll();
 		for(Seller obj : list) {
-			System.out.println("TESTE 03 " + obj);
+			System.out.println("TESTE 03 - " + obj);
 		}
+		
+		Seller newSeller = new Seller(null, "Greg", "Greg@gmai.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+			System.out.println("TESTE 04 - " + newSeller.getId());
 	}
 }
