0d516e4fb465421bb784104403cfc57318817127
Saymon Botelho <82913820+saymonbot@users.noreply.github.com>
2025-07-31 18:54:53 -0300

    Update Program2.java

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -17,28 +17,28 @@ public static void main(String[] args) {
 
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		
-		System.out.println("TESTE 01 - FindById");
+		System.out.println("TEST 01 - FindById");
 		Department dep = departmentDao.findById(1);
 		System.out.println(dep);
 		
-		System.out.println("\nTESTE 02 - FindAll");
+		System.out.println("\nTEST 02 - FindAll");
 		List<Department> list = departmentDao.findAll();
 		for(Department obj : list) {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\nTESTE 03 - INSER DEPARTMENT");
+		System.out.println("\nTEST 03 - INSER DEPARTMENT");
 		Department department = new Department(null, "Cosmetics");
 		departmentDao.insert(department);
 		System.out.println(department.getId());
 		
-		System.out.println("\nTESTE 04 - UPDATE DEPARTMENT");
+		System.out.println("\nTEST 04 - UPDATE DEPARTMENT");
 		department = departmentDao.findById(1);
 		department.setName("Clothes");
 		departmentDao.update(department);
 		System.out.println(department.getId());
 		
-		System.out.println("\nTESTE 05 - DELETE DEPARTMENT");
+		System.out.println("\nTEST 05 - DELETE DEPARTMENT");
 		System.out.println("Enter id for delete");
 		int id = scan.nextInt();
 		departmentDao.deleteById(id);
