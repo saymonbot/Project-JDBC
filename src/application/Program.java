e819aeb8909ae134ecdccb8caf5572c25d911e48
Saymon Botelho <82913820+saymonbot@users.noreply.github.com>
2025-07-31 18:53:36 -0300

    Update Program.java

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,36 +17,36 @@ public class Program {
 		
 		//Program to execute alterations in seller table
 		
-		System.out.println("TESTE 01 - FindById");
+		System.out.println("TEST 01 - FindById");
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 		
-		System.out.println("\nTESTE 02 - FindByDepartment");
+		System.out.println("\nTEST 02 - FindByDepartment");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\nTESTE 03 - FindAll");
+		System.out.println("\nTEST 03 - FindAll");
 		list = sellerDao.findAll();
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\nTESTE 04 - INSERT SELLER");
+		System.out.println("\nTEST 04 - INSERT SELLER");
 		Seller newSeller = new Seller(null, "Greg", "Greg@gmai.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println(newSeller.getId());
 			
-		System.out.println("\nTESTE 05 - UPDATE SELLER");
+		System.out.println("\nTEST 05 - UPDATE SELLER");
 		seller = sellerDao.findById(1);
 		seller.setName("Martha Wayne");
 		sellerDao.update(seller);
 		System.out.println(seller.getId());
 		
-		System.out.println("\nTESTE 06 - DELETE SELLER");
+		System.out.println("\nTEST 06 - DELETE SELLER");
 		System.out.println("Enter id for delete");
 		int id = scan.nextInt();
 		sellerDao.deleteById(id);
